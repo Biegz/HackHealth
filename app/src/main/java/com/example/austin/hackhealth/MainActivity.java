04be84bf3d708705fce08496356f4faad4efd228
austin <biega27@mail.sunysuffolk.edu>
2018-02-03 15:37:22 -0500

    some controller changes
    still trying to access the text views

--- app/src/main/java/com/example/austin/hackhealth/MainActivity.java
+++ app/src/main/java/com/example/austin/hackhealth/MainActivity.java
@@ -1,9 +1,6 @@
 package com.example.austin.hackhealth;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -12,12 +9,32 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import controller.StepController;
+import stepcookieBackend.Achievement;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        Achievement achievement = new Achievement() {
+            @Override
+            public int step() {
+                return 0;
+            }
+
+            @Override
+            public double progress() {
+                return 0;
+            }
+        };
+
+        StepController stepController = new StepController(achievement);
+        //stepController.updateView();
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
--- app/src/test/java/Controller/StepController.java
+++
@@ -1,8 +0,0 @@
-package Controller;
-
-/**
- * Created by austin on 2/3/18.
- */
-
-public class StepController {
-}
