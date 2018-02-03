e56114f289b963f74b7892920635429e9e66ae02
Dan <dbaccs@gmail.com>
2018-02-03 12:58:16 -0500

    First Backend Update

---
+++ app/src/main/java/stepcookieBackend/Multiplier.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package stepcookieBackend;
+
+/**
+ *
+ * This class will be used for repeating rewards
+ * 
+ * @author Dan
+ */
+public abstract class Multiplier implements Steppable{
+    
+    long internalSteps;
+    String description;
+    String name;
+    //ImageView 
+    
+    
+}
